257de1c121d93a0e072e1f6a9d1cd853161ec2b8
George Dean <gdean123@gmail.com>
2014-12-07 18:17:32 -0800

    Extract ContainedThread class

--- test.rb
+++ test.rb
@@ -1,22 +1,37 @@
 require 'pty'
 require 'thread'
 
-journey_test_output = Queue.new
-journey_tests_passed = false
+class ContainedThread
+  attr_reader :succeeded
 
-journey_test_thread = Thread.start do
-  PTY.spawn('ruby journeys_test.rb') do |stdout, stdin, pid|
-    stdout.each do |line|
-      journey_test_output.enq(line)
+  def initialize
+    @output = Queue.new
+    @succeeded = false
+  end
+
+  def start(command)
+    @thread = Thread.start do
+      PTY.spawn(command) do |stdout, _, pid|
+        stdout.each do |line|
+          @output.enq(line)
+        end
+        @succeeded = (Process.wait2(pid)[1].exitstatus == 0)
+      end
     end
-    journey_tests_passed = (Process.wait2(pid)[1].exitstatus == 0)
+  end
+
+  def blocking_print
+    while @thread.alive? || !@output.empty? do
+      puts @output.pop unless @output.empty?
+    end
   end
 end
 
-java_tests_passed = system "ruby java_test.rb"
+journey_test_thread = ContainedThread.new
+journey_test_thread.start('ruby journeys_test.rb')
 
-while journey_test_thread.alive? || !journey_test_output.empty? do
-  puts journey_test_output.pop unless journey_test_output.empty?
-end
+java_tests_passed = system 'ruby java_test.rb'
 
-exit journey_tests_passed && java_tests_passed
+journey_test_thread.blocking_print
+
+exit journey_test_thread.succeeded && java_tests_passed
