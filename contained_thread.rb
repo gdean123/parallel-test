79cb4e3c3111d58309069e3903eef011bf216b81
George Dean <gdean123@gmail.com>
2014-12-07 18:39:41 -0800

    Extract contained_thread.rb

---
+++ contained_thread.rb
@@ -0,0 +1,43 @@
+require 'pty'
+require 'thread'
+
+class ContainedThread
+  attr_reader :succeeded
+
+  def self.start(command)
+    contained_thread = ContainedThread.new
+    contained_thread.start(command)
+    contained_thread
+  end
+
+  def initialize
+    @output = Queue.new
+  end
+
+  def start(command)
+    @thread = Thread.start do
+      PTY.spawn(command) do |stdout, _, pid|
+        enqueue_output(stdout)
+        @succeeded = exit_success?(pid)
+      end
+    end
+  end
+
+  def blocking_print
+    while @thread.alive? || !@output.empty? do
+      puts @output.pop unless @output.empty?
+    end
+  end
+
+  private
+
+  def enqueue_output(stdout)
+    stdout.each do |line|
+      @output.enq(line)
+    end
+  end
+
+  def exit_success?(pid)
+    Process.wait2(pid)[1].exitstatus == 0
+  end
+end
--- test.rb
+++ test.rb
@@ -1,46 +1,4 @@
-require 'pty'
-require 'thread'
-
-class ContainedThread
-  attr_reader :succeeded
-
-  def self.start(command)
-    contained_thread = ContainedThread.new
-    contained_thread.start(command)
-    contained_thread
-  end
-
-  def initialize
-    @output = Queue.new
-  end
-
-  def start(command)
-    @thread = Thread.start do
-      PTY.spawn(command) do |stdout, _, pid|
-        enqueue_output(stdout)
-        @succeeded = exit_success?(pid)
-      end
-    end
-  end
-
-  def blocking_print
-    while @thread.alive? || !@output.empty? do
-      puts @output.pop unless @output.empty?
-    end
-  end
-
-  private
-
-  def enqueue_output(stdout)
-    stdout.each do |line|
-      @output.enq(line)
-    end
-  end
-
-  def exit_success?(pid)
-    Process.wait2(pid)[1].exitstatus == 0
-  end
-end
+require_relative 'contained_thread'
 
 journey_test_thread = ContainedThread.start('ruby journeys_test.rb')
 
