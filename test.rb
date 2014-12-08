2970a42b0a3c12004b1741a004507736453416c4
George Dean <gdean123@gmail.com>
2014-12-07 18:59:49 -0800

    Use ContainedThread for all tests

--- test.rb
+++ test.rb
@@ -1,9 +1,9 @@
 require_relative 'contained_thread'
 
-journey_test_thread = ContainedThread.start('ruby journeys_test.rb')
+journey_tests = ContainedThread.start('ruby journeys_test.rb')
+java_tests = ContainedThread.start('ruby java_test.rb')
 
-java_tests_passed = system 'ruby java_test.rb'
+java_tests.blocking_print
+journey_tests.blocking_print
 
-journey_test_thread.blocking_print
-
-exit journey_test_thread.succeeded && java_tests_passed
+exit journey_tests.succeeded && java_tests.succeeded
